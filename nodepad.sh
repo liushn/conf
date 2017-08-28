9cd3b0314180addb38c700d11006eb161b771e62
liushn <liushn@outlook.com>
2017-08-28 23:29:03 +0800

    1

--- nodepad.sh
+++ nodepad.sh
@@ -6,3 +6,6 @@ xclip -selection c  id_rsa.pub
 #git基本设置
 git config --global user.name "name"
 git config --global user.email "name@example.com"
+#npm
+npm config set registry https://registry.npm.taobao.org
+npm install sinopia
